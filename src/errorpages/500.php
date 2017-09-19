d9affea632822a3c74a6a9f67eb1ac5ca197e047
PhiliPdB <phlpdbrn@gmail.com>
2017-09-19 20:00:51 +0200

    Add 500 error page

---
+++ src/errorpages/500.php
@@ -0,0 +1,49 @@
+<?php
+// Enable PHP Gzip compression
+ob_start("ob_gzhandler");
+
+function version($file) {
+    return $file . '?' . filemtime($file);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="robots" content="noindex, follow" />
+
+    <title>500 - Internal Server Error</title>
+
+    <link rel="stylesheet" href="<?=version("../css/main.css")?>">
+
+    <!-- Favicons -->
+    <?php include("../favicons.html") ?>
+</head>
+<body>
+<!-- Header -->
+<?php require("../components/header.php"); ?>
+
+<!-- Banner -->
+<div id="home">
+    <div class="background_image"></div>
+    <div id="title" class="center">
+        <h1>Internal Server Error</h1>
+    </div>
+</div>
+
+<div class="card">
+    <h2>500 - Internal Server Error</h2>
+    <p>
+        Unfortunately, the server failed to perform the task it was requested to do.
+    </p>
+</div>
+
+<!-- Footer -->
+<?php require("../components/footer.php"); ?>
+
+<!-- Scripts -->
+<script src="<?=version("../js/script.js")?>" type="text/javascript" charset="utf-8" async defer></script>
+</body>
+</html>
